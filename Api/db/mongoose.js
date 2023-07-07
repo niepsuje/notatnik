8231996842de55c5c8ea077a6ecb671472e21bf2
niepsuje <60855281+niepsuje@users.noreply.github.com>
2023-07-07 11:17:03 +0200

    Poprawa do poprzedniego commit’a: ->dodanie parametrów. ->doinstalowanie mongoose; Model notatki, notatka testowa i zapis do bazy

--- Api/db/mongoose.js
+++ Api/db/mongoose.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
 // db connect
-mongoose.connect('ścieżka/do/naszej/bazy', {opcjonalne ustawienia});
+mongoose.connect('mongodb://127.0.0.1:27017/notes-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
+// model notatki
+const Note = mongoose.model('Note', {
+    title: String,
+    body: String
+});
+
+// notatka testowa i zapisanie do bazy
+const newNote = new Note({ 
+    title: 'tytuł testowy',
+    body: 'tekst naszej notatki'
+});
+newNote. save().then(() => {
+    console.log('notatka została zapisana')
+});
--- Api/index.js
+++ Api/index.js
@@ -3,10 +3,13 @@ const app = express();
 const { port } = require('./config');
 const apiRouter = require('./routes/api')
 
+// db
+require('./db/mongoose'); 
+
 // routers
-app.use('/', apiRouter);
+// app.use('/', apiRouter);
 
 // server
 app.listen(port, () => {
     console.log('server słucha... http://localhost' + port);
-});
+}); 
