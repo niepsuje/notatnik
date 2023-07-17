2990bc02e3b17510b60e7384e7853ca2036c9f2c
niepsuje <60855281+niepsuje@users.noreply.github.com>
2023-07-17 12:10:01 +0200

    1. Usuwanie notatki - React; 2. Przekazanie funkcji deleteNote do danego przycisku - tak jak każdą inną wartość; 3.a. Odświeżanie tablicy po kliknięciu na usuń - przerzucamy wartość do specjalnego obiektu Reacta o nazwie State. Aktualizuje wszystkie pola na this.state.notes…; 3.b. Używam specjalnej funkcji Reacta this.setState({ notes }) i mówię jaki Stan/wartość chce nadpisać? Chcę ją nadpisać nową wartością notes; 4. Każdy element po wszystkich notatkach powinien mieć jakiś klucz - key - żeby react mógł sobie rozpoznać poszczególne elementy

--- Api/frontend/front-react-app/src/components/Notes/Note/Note.js
+++ Api/frontend/front-react-app/src/components/Notes/Note/Note.js
@@ -1,12 +1,19 @@
+// komponent notatki
 import React from 'react';
 
 function Note(props) {
+
+      // przekazuje z dziecka id do komponentu Notes
+
     return (
         <div className="note">
             <p>{props.title}</p>
             <div className='description'>{props.body}</div>
             <button>edytuj</button>
-            <button className="delete">usuń</button>
+            <button 
+                className="delete" 
+                onClick={() => props.onDelete(props.id)}>usuń 
+            </button>   
         </div>
     );
 }
--- Api/frontend/front-react-app/src/components/Notes/Notes.js
+++ Api/frontend/front-react-app/src/components/Notes/Notes.js
@@ -3,22 +3,31 @@ import './Notes.css';
 import Note from './Note/Note';
 
 // komponent klasowy
+
 class Notes extends React.Component {
     constructor(props) {
         super(props);
+        ///
+        this.state = {
+            notes: [
+                {
+                    id: '2323',
+                    title: 'Wykąpać psa',
+                    body: 'pamiętaj żeby umyć Simbę specjalnym szamponem.'
+                },
+                {
+                    id: '4234',
+                    title: 'Zrobić zakupy',
+                    body: 'kupić mleko, masło, jaja, orzechy'
+                }
+            ]
+        };
+    }
 
-        this.notes = [
-            {
-                id: '2323',
-                title: 'Wykąpać psa',
-                body: 'pamiętaj żeby umyć Simbę specjalnym szamponem.'
-            },
-            {
-                id: '4234',
-                title: 'Zrobić zakupy',
-                body: 'kupić mleko, masło, jaja, orzechy'
-            }
-        ];
+    deleteNote(id) {
+        console.log('usuwanie notatki: ', id);
+        const notes = [...this.state.notes].filter(note => note.id !== id);   // id inne niż te które podam w parametrze
+        this.setState({ notes });
     }
 
     render() {
@@ -27,12 +36,14 @@ class Notes extends React.Component {
         <div>
             <p>Moje notatki:</p>
 
-            {this.notes.map(note => (
+            {this.state.notes.map(note => (
                 <Note 
+                    key={note.id}
                     title={note.title}
                     body={note.body}
                     id={note.id}
-                    />
+                    onDelete={(id) => this.deleteNote(note.id)}  />  // przekazuje funkcję deleteNote
+               
             ))}
 
 
